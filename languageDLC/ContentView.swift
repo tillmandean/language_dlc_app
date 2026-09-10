4c381184ca1873d8f1c1eeb1842754f6a404c28b
Tillman Dean <tillman.dean2000@gmail.com>
2026-09-10 18:56:57 -0400

    Initial Commit

---
+++ languageDLC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  languageDLC
+//
+//  Created by Tillman Dean on 9/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ languageDLC/languageDLCApp.swift
@@ -0,0 +1,17 @@
+//
+//  languageDLCApp.swift
+//  languageDLC
+//
+//  Created by Tillman Dean on 9/10/26.
+//
+
+import SwiftUI
+
+@main
+struct languageDLCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
